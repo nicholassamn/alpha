a368766997ff6f1b845b99f3691f369b5ba21f32
Nicholas Kevin <nicholaskevin2008@gmail.com>
2026-01-19 06:07:05 +0000

    1.2

---
+++ game.py
@@ -0,0 +1,12 @@
+# Aplikasi game secret number
+
+secret_number = 777
+
+guess_number = int(input("Masukkan Tebak Angka:"))
+
+while guess_number != secret_number
+    print("Tebakan salah, silakan coba lagi!")
+    guess_number = int(input("Masukkan Tebak Angka:"))
+
+print("Tebakan anda Benar! Selamat")
+print("Kode ini saya buat di codespace")
