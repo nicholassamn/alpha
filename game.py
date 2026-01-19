6d14772e94145e9d64952b7e746f3782c0d9e4b3
Nicholas Kevin <nicholaskevin2008@gmail.com>
2026-01-19 06:11:55 +0000

    1.3

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 # Aplikasi game secret number
 
 secret_number = 777
-
 guess_number = int(input("Masukkan Tebak Angka:"))
 
 while guess_number != secret_number
     print("Tebakan salah, silakan coba lagi!")
+    print("Anda terjebak dalam perputaran abadi") #modify
     guess_number = int(input("Masukkan Tebak Angka:"))
 
 print("Tebakan anda Benar! Selamat")
